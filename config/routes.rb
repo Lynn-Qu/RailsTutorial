bc2529a3538bfc6484e4c2489abecab3eabd4ea5
Lynn <qhl-96@tamu.edu>
2020-10-17 02:40:47 +0000

    finished toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+  # this line will map the relationship between webpage and url.
+  # each url will correpond to the specific actions in the users controller
+  resources :users
+
+  # root 'controller_name#action_name'
+  root 'users#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
